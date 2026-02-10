b27ad4c8675e573ebc132fcaa9d4595d541e52e9
Sulma-Dubon <bioinformaticasd@gmail.com>
2026-02-10 18:53:12 +0100

    Fix NFC configure route order

--- nfc/urls.py
+++ nfc/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import PublicNFCView, NFCConfigureView
 
 urlpatterns = [
-    path('<str:public_uid>/', PublicNFCView.as_view(), name='public-nfc'),
     path('configure/', NFCConfigureView.as_view(), name='nfc-configure'),
+    path('<str:public_uid>/', PublicNFCView.as_view(), name='public-nfc'),
 ]

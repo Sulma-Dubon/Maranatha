9b5bc005a1d2ff93f64a46df725d554cc2ccf8d3
Sulma-Dubon <bioinformaticasd@gmail.com>
2026-02-23 08:32:23 +0100

    Revert "Add social feed MVP in separate app"
    This reverts commit 928cbbe9a58d2e68fdaeaca19c392f49486644bf.

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'content',
     'nfc',
     'experiences',
-    'social',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/nfc/', include('nfc.urls')),
-    path('api/', include('social.urls')),
     path('api/', include('experiences.urls')),
     path('', include('experiences.web_urls')),
 ]

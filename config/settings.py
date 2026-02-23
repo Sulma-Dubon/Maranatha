e7f518f93fb0504d269664d1941895547e2a67bb
Sulma-Dubon <bioinformaticasd@gmail.com>
2026-02-23 08:27:57 +0100

    Add social feed MVP in separate app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'content',
     'nfc',
     'experiences',
+    'social',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/nfc/', include('nfc.urls')),
+    path('api/', include('social.urls')),
     path('api/', include('experiences.urls')),
     path('', include('experiences.web_urls')),
 ]
